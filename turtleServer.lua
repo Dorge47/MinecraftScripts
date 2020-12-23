44d3a814e77592d01e758a217585b6e175171ed9
Dorge47 <Dorge47@users.noreply.github.com>
2020-12-22 23:06:38 -0800

    Change steps to number and add remaining movement

--- turtleServer.lua
+++ turtleServer.lua
@@ -23,7 +23,7 @@ function waitForInstruct(port)
 	elseif string.find(message,"forward") then
 		local stepsForward = 1
 		if string.find(message," ") then
-			stepsForward = Split(message," ")[2]
+			stepsForward = tonumber(Split(message," ")[2])
 		end
 		if turtle.getFuelLevel() < stepsForward then
 			local modem = peripheral.wrap(getModem())
@@ -32,6 +32,66 @@ function waitForInstruct(port)
 			for x = 1, stepsForward, 1 do
 				turtle.forward()
 			end
+		end
+		waitForInstruct(port)
+	elseif string.find(message,"back") then
+		local stepsBack = 1
+		if string.find(message," ") then
+			stepsBack = tonumber(Split(message," ")[2])
+		end
+		if turtle.getFuelLevel() < stepsBack then
+			local modem = peripheral.wrap(getModem())
+			modem.transmit(replyChannel,senderChannel,"Not enough fuel")
+		else
+			for x = 1, stepsBack, 1 do
+				turtle.back()
+			end
+		end
+		waitForInstruct(port)
+	elseif string.find(message,"up") then
+		local stepsUp = 1
+		if string.find(message," ") then
+			stepsUp = tonumber(Split(message," ")[2])
+		end
+		if turtle.getFuelLevel() < stepsUp then
+			local modem = peripheral.wrap(getModem())
+			modem.transmit(replyChannel,senderChannel,"Not enough fuel")
+		else
+			for x = 1, stepsUp, 1 do
+				turtle.up()
+			end
+		end
+		waitForInstruct(port)
+	elseif string.find(message,"down") then
+		local stepsDown = 1
+		if string.find(message," ") then
+			stepsDown = tonumber(Split(message," ")[2])
+		end
+		if turtle.getFuelLevel() < stepsDown then
+			local modem = peripheral.wrap(getModem())
+			modem.transmit(replyChannel,senderChannel,"Not enough fuel")
+		else
+			for x = 1, stepsDown, 1 do
+				turtle.down()
+			end
+		end
+		waitForInstruct(port)
+	elseif string.find(message,"left") then
+		local stepsLeft = 1
+		if string.find(message," ") then
+			stepsLeft = tonumber(Split(message," ")[2])
+		end
+		for x = 1, stepsLeft, 1 do
+			turtle.turnLeft()
+		end
+		waitForInstruct(port)
+	elseif string.find(message,"right") then
+		local stepsRight = 1
+		if string.find(message," ") then
+			stepsRight = tonumber(Split(message," ")[2])
+		end
+		for x = 1, stepsRight, 1 do
+			turtle.turnRight()
 		end
 		waitForInstruct(port)
 	elseif string.find(message,"stop") then
